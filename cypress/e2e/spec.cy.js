9ab8ba82ff5d114dc5dfe727b2488e453ed754c1
Marco Tulio Valente <mtvalente@gmail.com>
2024-06-14 09:25:08 -0300

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,62 @@
+describe('template spec', () => {
+  it('Verifica se app está abrindo', () => {
+    cy.visit('http://127.0.0.1:7001/')
+  })
+
+  it('Insere uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo') // seleciona input para inserir tarefas
+      .type('TP2 de Engenharia de Software{enter}'); // digita tarefa
+
+    cy.get('.todo-list li') // seleciona lista de todos
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software'); 
+  });
+
+  it('Insere e deleta uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001'); // Visit the TodoMVC app
+
+    cy.get('.new-todo')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('.todo-list li .destroy')
+      .invoke('show')
+      .click();
+
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
+
+  it('Filtra tarefas completas e ativas', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    // Seleciona apenas tarefas ativas
+    cy.contains('Active').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+
+    // Seleciona apenas tarefas completas
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    // Show all todos
+    cy.contains('All').click();
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+  });
+});
