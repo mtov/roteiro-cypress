af85cb66ca8e47a891f65d52825d5a5f18fac3b9
Ian Guelman <ianguelman@gmail.com>
2024-11-07 00:09:42 -0300

    Implement Best Practices

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,59 +1,71 @@
-describe('template spec', () => {
+describe('TODOMvc App', () => {
   it('Verifica se app está abrindo', () => {
-    cy.visit('http://127.0.0.1:7001/')
+    cy.visit('')
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit('http://127.0.0.1:7001'); 
+    cy.visit(''); 
 
-    cy.get('.new-todo')
+    cy.get('[data-cy=todo-input]')
       .type('TP2 de Engenharia de Software{enter}');
 
-    cy.get('.todo-list li')
+    cy.get('[data-cy=todos-list]')
+      .children()
       .should('have.length', 1) 
       .first()
       .should('have.text', 'TP2 de Engenharia de Software'); 
   });
 
   it('Insere e deleta uma tarefa', () => {
-    cy.visit('http://127.0.0.1:7001');
+    cy.visit('');
 
-    cy.get('.new-todo')
+    cy.get('[data-cy=todo-input]')
       .type('TP2 de Engenharia de Software{enter}');
 
-    cy.get('.todo-list li .destroy')
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
       .invoke('show')
       .click();
 
-    cy.get('.todo-list li')
+    cy.get('[data-cy=todos-list]')
+      .children()
       .should('have.length', 0);
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit('http://127.0.0.1:7001'); 
+    cy.visit(''); 
 
-    cy.get('.new-todo')
+    cy.get('[data-cy=todo-input]')
       .type('TP2 de ES{enter}')
       .type('Prova de ES{enter}');
 
-    cy.get('.todo-list li .toggle')
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
       .first()
       .click();
 
-    cy.contains('Active').click();
-    cy.get('.todo-list li')
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
       .should('have.length', 1)
       .first()
       .should('have.text', 'Prova de ES');
 
-    cy.contains('Completed').click();
-    cy.get('.todo-list li')
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
       .should('have.length', 1)
       .first()
       .should('have.text', 'TP2 de ES');
 
-    cy.contains('All').click();
-    cy.get('.todo-list li')
+    cy.get('[data-cy=filter-all-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
       .should('have.length', 2);
   });
 });
